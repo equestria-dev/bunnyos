c19cc95c1a2d19c6d6a567e7cf84308e87346640
Starscouts <starscouts@equestria.dev>
2024-09-14 14:18:52 +0200

    Rewrite build script for Linux

--- build.ps1
+++
@@ -1,6 +0,0 @@
-cd ./os
-if ($?) { cargo build --target x86_64-unknown-uefi } else { cd ..; exit 1 }
-if ($?) { cd ../tools } else { cd ..; exit 1 }
-if ($?) { cargo build } else { cd ..; exit 1 }
-if ($?) { cd .. } else { exit 1 }
-if ($?) { ./tools/target/debug/mkbimg.exe } else { exit 1 }
